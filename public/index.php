e6ce235b8a885982138fc28248475197b5bb90e6
tadiotc <tadiotc@gmail.com>
2022-12-04 20:36:42 -0300

    homecontroller

--- public/index.php
+++ public/index.php
@@ -7,9 +7,6 @@ require '../vendor/autoload.php';
 session_start();
 
 // dd($_SERVER);
-
-dd(RequestType::get());
-
-// dd( trim( parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH )) );
+// dd(RequestType::get());
 
 Router::run();
